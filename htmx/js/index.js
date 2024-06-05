f90945ce5fb9f22f82c021509a4a18a82404019c
minor <focus.minor@gmail.com>
2024-06-05 11:00:52 +0500

    gitignore

---
+++ htmx/js/index.js
@@ -0,0 +1,3 @@
+import func from "./module/test.js";
+
+func();
---
+++ htmx/js/module/test.js
@@ -0,0 +1,8 @@
+function func() {
+  console.log(
+    "%c Hello Best Template :)",
+    "background-color: lime; color: black; "
+  );
+}
+
+export default func;
